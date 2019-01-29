a96f31fef0d59950434e1815274ec3f6ed00efe1
Julio Ansede <julioansede@gmail.com>
2019-01-29 17:44:16 +0100

    Primer dia

---
+++ Dia1/app.js
@@ -0,0 +1,25 @@
+/*
+ * Aquí podéis hacer los ejercicios y
+ * practicar Javascript!
+ */
+//hoisting
+/*console.log("Probando, probando, 1, 2, 3!");
+console.log(object);
+[1, 2].forEach(alert);*/
+/*let edad = 38;
+let nombre = "Julio";
+const cumple;
+alert("Mi nombre es " + nombre + " y tengo " + edad);
+console.log(`Mi nombre<
+es ${nombre}`);
+console.log(`mi nombre es ${1+1}  + nombre`)
+const COLOR_ORANGE= "#FF7F00";*/
+
+/*let admin;
+let name;
+name = "jhon";
+admin = name;
+alert(admin);*/
+
+/*let ourPlanetName= "earth";
+let currentVisitorName;*/
